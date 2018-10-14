692a3e8f13dc015904261e49c9967535bb12800f
martinberthier <berthier.martin@gmail.com>
2018-10-14 16:48:38 +0200

    exo 3 terminé

--- exos tableaux/src/Tableau3.java
+++ exos tableaux/src/Tableau3.java
@@ -1,14 +1,54 @@
 
+import java.util.*;
+import java.util.Arrays;
+
 public class Tableau3 {
 
 	public static void main(String[] args) {
 		int [] tableau = {1, 5, 25};
 		
-		for(int i = 0 ; i < tableau.length ; i++)
-		{
-		    Ville v = tableau[i];
-		    System.out.println(v.decrisToi()+"\n");
-		}
+//		boolean contain= false;
+//		int valeurATrouver=25;
+//
+//			for(int i=0; i<tableau.length; i++)
+//	        {
+//	            if(tableau[i]==valeurATrouver)
+//	            {
+//	                contain=true;
+//	            }
+//	        }
+//	        if(contain){
+//	        	System.out.println("La valeur "+valeurATrouver+" est comprise dans le tableau");
+//	        	}else{
+//	        		System.out.println("La valeur "+valeurATrouver+" n'est pas comprise dans le tableau");
+//	        		}
+		
+			int valeurATrouver = 25;
+		
+			for(int i = 0 ; i < tableau.length ; i++){
+				if (tableau[i] == valeurATrouver) {
+					System.out.println("Le tableau contient "+valeurATrouver);
+				}
+			}
+		
+			
+//			if(Arrays.asList(tableau).contains(toFind)) {
+//				System.out.println("Le tableau contient "+toFind);
+//			}
+			Arrays.sort(tableau);
+			int toFind = 5;
+			
+			int searchValue = Arrays.binarySearch(tableau,toFind);
+			System.out.println("Le tableau contient "+toFind);
+			
+//			public static int binarySearch(int[] tableau, int toFind){
+//				System.out.println("Le tableau contient "+toFind);
+			}
+
 	}
 
-}
+
+
+
+
+
